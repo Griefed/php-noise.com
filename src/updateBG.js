ac431056f90b9af4be0590fe9576f6ae0ff82613
Griefed <griefed@griefed.de>
2021-07-14 14:47:09 +0000

    Deploying to gh-pages from @ Griefed/php-noise.com@bcc4d04bb4ce60b9d5596926ed29a13ac756dbda 🚀

---
+++ src/updateBG.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// NullDev 4 RundesBalli <3
+
+let changeBg = (r, g, b, hex, tiles, tileSize, borderWidth) => {
+    fetch(`https://php-noise.com/noise.php?r=${r}&g=${g}&b=${b}&hex=${hex}&tiles=${tiles}&tileSize=${tileSize}&borderWidth=${borderWidth}&json`, { mode: "cors" })
+        .then(res => res.json())
+        .then(data => {
+            document.body.style.background = `url("${data.uri}") repeat center center`;
+            document.getElementById("download").style.display = "block";
+            document.getElementById("downloadlink").setAttribute("href", data.uri);
+        });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("submit").addEventListener("click", (e) => {
+        e.preventDefault();
+        changeBg(
+            document.getElementById("r").value,
+            document.getElementById("g").value,
+            document.getElementById("b").value,
+            document.getElementById("hex").value,
+            document.getElementById("tiles").value,
+            document.getElementById("tileSize").value,
+            document.getElementById("borderWidth").value
+        );
+    });
+});
